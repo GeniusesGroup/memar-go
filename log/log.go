1e42f82c9b898184c409799b8953dcc8932bb6f5
Omid <omid@sabz.city>
2020-10-22 11:21:31 +0330

    Improve log package >> - Add two const to more control how log print and save! - Temporary use os.File to save logs to storage to prevent log loses - Add abbility to split log records by desire interval to prevent huge one log file!

--- log/log.go
+++ log/log.go
@@ -4,63 +4,97 @@ package log
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path"
+	"strconv"
 	"time"
+
+	etime "../earth-time"
+)
+
+const (
+	// DevMode use to show more log when enabled and disabled||enabled some rules!
+	DevMode = false
+	// DebugMode use to show more log when enabled!
+	DebugMode = false
 )
 
 const (
 	timeFormat = "2006-01-02 15:04:05"
 )
 
-// TODO::: fix problem with multi CPU core parallelism
+// TODO::: fix problem with multi CPU core parallelism (data race condition)
 
 // hold logs until app running.
-var buffer = make([]byte, 0, 8192)
+var logFile *os.File
+
+// Init will initialize log to do some interval saving
+func Init(name, repoLocation string, interval int64) (err error) {
+	var logFolder = path.Join(repoLocation, "log")
+	os.Mkdir(logFolder, 0700)
+	logFile, err = os.OpenFile(path.Join(logFolder, name+strconv.FormatInt(etime.RoundSeconds(etime.Now(), interval), 10)+".log"), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0700)
+	if err != nil {
+		return
+	}
+	go intervalSaving(name, repoLocation, interval)
+	return
+}
 
 // Debug show log in standard console & append log to buffer to save them later.
 func Debug(a ...interface{}) {
 	var log = fmt.Sprintln("[Debug]", time.Now().Format(timeFormat), a)
-	// write log to stderr
-	os.Stderr.WriteString(log)
-	// Append log to buffer for saving by SaveToStorage() later.
-	buffer = append(buffer, log...)
+	if DevMode {
+		os.Stderr.WriteString(log)
+	}
+	logFile.WriteString(log)
 }
 
 // Info show log in standard console & append log to buffer to save them later.
 func Info(a ...interface{}) {
 	var log = fmt.Sprintln("[Info]", time.Now().Format(timeFormat), a)
-	// write log to stderr
-	os.Stderr.WriteString(log)
-	// Append log to buffer for saving by SaveToStorage() later.
-	buffer = append(buffer, log...)
+	if DevMode {
+		os.Stderr.WriteString(log)
+	}
+	logFile.WriteString(log)
 }
 
 // Warn show log in standard console & append log to buffer to save them later.
 func Warn(a ...interface{}) {
 	var log = fmt.Sprintln("[Warn]", time.Now().Format(timeFormat), a)
-	// write log to stderr
-	os.Stderr.WriteString(log)
-	// Append log to buffer for saving by SaveToStorage() later.
-	buffer = append(buffer, log...)
+	if DevMode {
+		os.Stderr.WriteString(log)
+	}
+	logFile.WriteString(log)
 }
 
 // Fatal show log in standard console & append log to buffer to save them later and exit app.
 func Fatal(a ...interface{}) {
 	var log = fmt.Sprintln("[Fatal]", time.Now().Format(timeFormat), a)
-	// write log to stderr
-	os.Stderr.WriteString(log)
-	// Append log to buffer for saving by SaveToStorage() later.
-	buffer = append(buffer, log...)
+	if DevMode {
+		os.Stderr.WriteString(log)
+	}
+	logFile.WriteString(log)
 	panic("Due to important log, panic situation occur")
 }
 
-// SaveToStorage use to make||flush to {location}/{name}.log if needed!
-func SaveToStorage(name, location string) {
-	// TODO::: Check if log file not exist create it || Flush old logs!
-	err := ioutil.WriteFile(path.Join(location, name+".log"), buffer, 0755)
-	if err != nil {
-		panic(fmt.Sprintf("Unable to write log file: %v", err))
+// SaveToStorage use to make||flush file!
+func SaveToStorage() {
+	logFile.Close()
+}
+
+func intervalSaving(name, location string, interval int64) {
+	var timer = time.NewTimer(time.Duration(etime.UntilRoundSeconds(etime.Now(), interval)) * time.Second)
+	for {
+		select {
+		// case shutdownFeedback := <-pcs.shutdownSignal:
+		// 	timer.Stop()
+		// 	shutdownFeedback <- struct{}{}
+		// 	return
+		case <-timer.C:
+			logFile.Close()
+
+			logFile, _ = os.OpenFile(path.Join(location, name+strconv.FormatInt(etime.RoundSeconds(etime.Now(), interval), 10)+".log"), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0700)
+			timer.Reset(time.Duration(interval) * time.Second)
+		}
 	}
 }
