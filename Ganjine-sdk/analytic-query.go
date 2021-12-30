431327306bfb09c173bd271db5ad9d834cf9653c
Omid <omid@sabz.city>
2021-12-30 11:16:48 +0330

    [ganjine] Delete services but implement them later in protocol way

--- Ganjine-sdk/analytic-query.go
+++
@@ -1,5 +0,0 @@
-/* For license and copyright information please see LEGAL file in repository */
-
-package gsdk
-
-// It is async query that run beside normal query from exiting database and get long time awnser ready!
