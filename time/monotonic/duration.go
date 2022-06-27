3a98da3292352dbe3e7b181bbaeb0329904ae285
Omid Hekayati <omidhekayati@gmail.com>
2022-06-27 08:32:32 +0430

    [time] Remove some wrong duration from monotonic

--- time/monotonic/duration.go
+++ time/monotonic/duration.go
@@ -12,8 +12,4 @@ const (
 	Microsecond                   = 1000 * Nanosecond
 	Millisecond                   = 1000 * Microsecond
 	Second                        = 1000 * Millisecond
-	Minute                        = 60 * Second
-	Hour                          = 60 * Minute
-	Day                           = 24 * Hour
-	Week                          = 7 * Day
 )
