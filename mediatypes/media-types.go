52a18b9f2f9f981402698c3ff335fc21ff792b08
Omid Hekayati <omidhekayati@gmail.com>
2023-08-19 10:26:05 +0330

    [compress-types] improved to respect the protocol

--- mediatypes/media-types.go
+++ mediatypes/media-types.go
@@ -1,53 +1,70 @@
 /* For license and copyright information please see the LEGAL file in the code repository */
 
-package mediatype
+package mediatypes
 
 import (
-	"libgo/protocol"
+	"memar/protocol"
 )
 
-func RegisterMediaType(mt protocol.MediaType) {
-	// Check due to os can be nil almost in tests and benchmarks build
-	if protocol.OS != nil {
-		protocol.OS.RegisterMediaType(mt)
-	}
+func Register(mt protocol.MediaType) (err protocol.Error) { return mts.Register(mt) }
+func ByMediaType(mediaType string) (mt protocol.MediaType, err protocol.Error) {
+	return mts.GetByMediaType(mediaType)
+}
+func ByID(id protocol.MediaTypeID) (mt protocol.MediaType, err protocol.Error) {
+	return mts.GetByID(id)
+}
+func ByFileExtension(ex string) (mt protocol.MediaType, err protocol.Error) {
+	return mts.GetByFileExtension(ex)
 }
 
-// MediaTypes store all data structure details
-type MediaTypes struct{}
+var mts = mediaTypes{
+	poolByMediaType:     map[string]protocol.MediaType{},
+	poolByID:            map[protocol.MediaTypeID]protocol.MediaType{},
+	poolByFileExtension: map[string]protocol.MediaType{},
+}
+
+type mediaTypes struct {
+	poolByMediaType     map[string]protocol.MediaType
+	poolByID            map[protocol.MediaTypeID]protocol.MediaType
+	poolByFileExtension map[string]protocol.MediaType
+}
 
 // RegisterMediaType register given MediaType to the application
-func (dss *MediaTypes) RegisterMediaType(mt protocol.MediaType) {
+func (mts *mediaTypes) Register(mt protocol.MediaType) (err protocol.Error) {
+	// TODO::: change panics to error
 	if mt.MainType() == "" && mt.SubType() == "" {
 		panic("Mediatype doesn't has main and sub type. Can't register empty mediatype.")
 	}
-	register(mt)
-}
 
-func (dss *MediaTypes) GetMediaTypeByID(id protocol.MediaTypeID) protocol.MediaType { return ByID(id) }
-func (dss *MediaTypes) GetMediaTypeByFileExtension(ex string) protocol.MediaType {
-	return ByFileExtension(ex)
-}
-func (dss *MediaTypes) GetMediaType(mediaType string) protocol.MediaType {
-	return ByMediaType(mediaType)
-}
-
-var (
-	poolByMediaType     = map[string]protocol.MediaType{}
-	poolByID            = map[protocol.MediaTypeID]protocol.MediaType{}
-	poolByFileExtension = map[string]protocol.MediaType{}
-)
-
-func ByMediaType(mediaType string) protocol.MediaType { return poolByMediaType[mediaType] }
-func ByID(id protocol.MediaTypeID) protocol.MediaType { return poolByID[id] }
-func ByFileExtension(ex string) protocol.MediaType    { return poolByFileExtension[ex] }
-
-func register(mt protocol.MediaType) {
 	// TODO::: lock??
-	poolByMediaType[mt.ToString()] = mt
-	poolByID[mt.ID()] = mt
+	mts.poolByMediaType[mt.ToString()] = mt
+	mts.poolByID[mt.ID()] = mt
 	var fe = mt.FileExtension()
 	if fe != "" {
-		poolByFileExtension[fe] = mt
+		mts.poolByFileExtension[fe] = mt
 	}
+	return
+}
+
+func (mts *mediaTypes) GetByMediaType(mediaType string) (mt protocol.MediaType, err protocol.Error) {
+	mt = mts.poolByMediaType[mediaType]
+	// if mt == nil {
+	// err = &errs.ErrNotFound
+	// }
+	return
+}
+
+func (mts *mediaTypes) GetByID(id protocol.MediaTypeID) (mt protocol.MediaType, err protocol.Error) {
+	mt = mts.poolByID[id]
+	// if mt == nil {
+	// err = &errs.ErrNotFound
+	// }
+	return
+}
+func (mts *mediaTypes) GetByFileExtension(ex string) (mt protocol.MediaType, err protocol.Error) {
+	mt = mts.poolByFileExtension[ex]
+	// if mt == nil {
+	// err = &errs.ErrNotFound
+	// }
+	return
 }
---
+++ mediatypes/media-types_test.go
@@ -0,0 +1,9 @@
+/* For license and copyright information please see the LEGAL file in the code repository */
+
+package mediatypes
+
+import (
+	"memar/protocol"
+)
+
+var _ protocol.MediaTypes = &mts
