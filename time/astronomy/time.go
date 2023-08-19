68b4b0d587f65d5175fbe9166daecdc56a6c5198
Omid Hekayati <omidhekayati@gmail.com>
2023-08-19 08:00:43 +0330

    [time] add two new epoch package

---
+++ time/astronomy/time.go
@@ -0,0 +1,7 @@
+/* For license and copyright information please see the LEGAL file in the code repository */
+
+package astronomy
+
+// https://en.wikipedia.org/wiki/Julian_year_(astronomy)
+// https://en.wikipedia.org/wiki/Epoch_(astronomy)
+// https://en.wikipedia.org/wiki/Terrestrial_Time
---
+++ time/mars/time.go
@@ -0,0 +1,5 @@
+/* For license and copyright information please see the LEGAL file in the code repository */
+
+package mars
+
+// https://en.wikipedia.org/wiki/Timekeeping_on_Mars
