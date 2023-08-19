5b817c7eabf3c8f0d216b2fd35b7b71e424a7037
Omid Hekayati <omidhekayati@gmail.com>
2023-08-19 09:03:41 +0330

    [service] improved to respect service protocol

--- service/service.go
+++ service/service.go
@@ -3,37 +3,23 @@
 package service
 
 import (
-	"libgo/protocol"
+	"memar/protocol"
 )
 
 // Service implement protocol.Service when embed to other struct that implements other needed methods.
 type Service struct{}
 
-//libgo:impl libgo/protocol.Service
-func (s *Service) URI() string                 { return "" }
-func (s *Service) Priority() protocol.Priority { return protocol.Priority_Unset }
-func (s *Service) Weight() protocol.Weight     { return protocol.Weight_Unset }
+//memar:impl memar/protocol.Service
+func (s *Service) ID() protocol.ServiceID { return 0 }
+
+//memar:impl memar/protocol.Service_Authorization
 func (s *Service) CRUDType() protocol.CRUD     { return protocol.CRUD_None }
 func (s *Service) UserType() protocol.UserType { return protocol.UserType_Unset }
 
-//libgo:impl libgo/protocol.ServiceDetails
-func (s *Service) Request() protocol.DataType { return nil }
-func (s *Service) Response() protocol.DataType  { return nil }
+//memar:impl memar/protocol.OperationImportance
+func (s *Service) Priority() protocol.Priority { return protocol.Priority_Unset }
+func (s *Service) Weight() protocol.Weight     { return protocol.Weight_Unset }
 
-/*
-*********** Handlers ***********
-not-implemented handlers of the service.
-*/
-
-func (s *Service) ServeSRPC(sk protocol.Socket) (err protocol.Error) {
-	err = &ErrServiceNotAcceptSRPC
-	return
-}
-func (s *Service) ServeSRPCDirect(sk protocol.Socket, request []byte) (response []byte, err protocol.Error) {
-	err = &ErrServiceNotAcceptSRPCDirect
-	return
-}
-func (s *Service) ServeHTTP(sk protocol.Socket, httpReq protocol.HTTPRequest, httpRes protocol.HTTPResponse) (err protocol.Error) {
-	err = &ErrServiceNotAcceptHTTP
-	return
-}
+//memar:impl memar/protocol.ServiceDetails
+func (s *Service) Request() protocol.DataType  { return nil }
+func (s *Service) Response() protocol.DataType { return nil }
