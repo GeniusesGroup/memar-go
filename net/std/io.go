d13aed907eed546005bce5ba9792d73744e4927a
Omid Hekayati <omidhekayati@gmail.com>
2023-09-23 13:35:38 +0330

    [net/tcp] move `std.io` to `std` package

--- net/std/io.go
+++ net/std/io.go
@@ -1,6 +1,6 @@
 /* For license and copyright information please see the LEGAL file in the code repository */
 
-package tcp
+package std
 
 import (
 	"io"
@@ -10,10 +10,10 @@ import (
 ********** protocol.Buffer interface **********
  */
 
-func (s *Stream) ReadFrom(reader io.Reader) (n int64, err error) { return }
-func (s *Stream) WriteTo(w io.Writer) (totalWrite int64, err error) {
+func (sk *Socket) ReadFrom(reader io.Reader) (n int64, err error) { return }
+func (sk *Socket) WriteTo(w io.Writer) (totalWrite int64, err error) {
 	var writeLen int
-	var data, _ = s.recv.buf.Marshal()
+	var data, _ = sk.Marshal()
 	writeLen, err = w.Write(data)
 	totalWrite = int64(writeLen)
 	return
