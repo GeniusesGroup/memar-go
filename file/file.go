3ac7ade7d69c93681189fe1b21b3383f7d983b97
Omid Hekayati <omidhekayati@gmail.com>
2023-11-23 17:43:53 +0330

    [file] Some fixes and Improvements

--- file/file.go
+++ file/file.go
@@ -1,4 +1,4 @@
-/* For license and copyright information please see LEGAL file in repository */
+/* For license and copyright information please see the LEGAL file in the code repository */
 
 package file
 
@@ -7,7 +7,9 @@ import (
 	"strconv"
 	"strings"
 
-	"../protocol"
+	cts "memar/compress-types"
+	"memar/minify"
+	"memar/protocol"
 )
 
 func FindByRelativeFrom(file protocol.File, relativePath string) (desireFile protocol.File) {
@@ -37,15 +39,99 @@ func FindByRelativeFrom(file protocol.File, relativePath string) (desireFile pro
 }
 
 func AddHashToFileName(file protocol.File) {
-	var nameWithoutExtension = file.MetaData().URI().NameWithoutExtension()
-	var fileExt = file.MetaData().URI().Extension()
+	var nameWithoutExtension = file.Metadata().URI().NameWithoutExtension()
+	var fileExt = file.Metadata().URI().Extension()
 
 	// Just want to differ two same file, So crc32 is more enough!
 	// var md5Hasher = md5.New()
-	// md5Hasher.Write(f.data)
+	// md5Hasher.Write(file.Data().Marshal())
 	// hash = hex.EncodeToString(md5Hasher.Sum(nil))
 	// md5Hasher.Reset()
-	var hashOfFileData = strconv.FormatUint(uint64(crc32.ChecksumIEEE(file.Data().Marshal())), 10)
+	var fileData, _ = file.Data().Marshal()
+	var hashOfFileData = strconv.FormatUint(uint64(crc32.ChecksumIEEE(fileData)), 10)
 
 	file.Rename(nameWithoutExtension + "-" + hashOfFileData + "." + fileExt)
+}
+
+// Minify replace file data with minify of them if possible.
+func Minify(file protocol.File) (err protocol.Error) {
+	err = minify.Minify(file.Data())
+	return
+}
+
+// Compress creates new files in the directory with desire compress algorithm
+func Compress(file protocol.File, contentEncodings []string, options protocol.CompressOptions) (err protocol.Error) {
+	// Check file type and compress just if it worth.
+	var fe = file.Metadata().URI().Extension()
+	switch fe {
+	case "png", "jpg", "gif", "jpeg", "mkv", "avi", "mp3", "mp4":
+		return
+	}
+
+	var parentDir = file.ParentDirectory()
+	var ct protocol.CompressType
+	for _, ce := range contentEncodings {
+		ct, err = cts.GetByContentEncoding(ce)
+		if err != nil {
+			continue
+		}
+		var compressor protocol.Codec
+		compressor, err = ct.Compress(file.Data(), options)
+		if err != nil {
+			return
+		}
+		var compressedFileName = file.Metadata().URI().Name() + "." + ct.FileExtension()
+		var compressedFile protocol.File
+		compressedFile, err = parentDir.File(compressedFileName)
+		if err != nil {
+			return
+		}
+		compressedFile.Data().Decode(compressor)
+	}
+	return
+}
+
+// ReplaceReq is request structure of Replace method.
+type ReplaceReq struct {
+	Data  string
+	Start int
+	End   int
+}
+
+// Replace replace given data in the file
+func ReplaceLocation(file protocol.File, data []ReplaceReq) (err protocol.Error) {
+	var fileData []byte
+	fileData, err = file.Data().Marshal()
+	if err != nil {
+		return
+	}
+
+	var totalAddedSize, addedSize int
+	for _, d := range data {
+		d.Start += totalAddedSize
+		d.End += totalAddedSize
+
+		var ln = len(d.Data)
+		addedSize = ln - (d.End - d.Start)
+		if addedSize > 0 {
+			if (cap(fileData) - len(fileData)) < addedSize {
+				fileData = append(fileData, make([]byte, addedSize)...)
+			} else {
+				// increase fileData len
+				fileData = fileData[:len(fileData)+addedSize]
+			}
+		}
+		totalAddedSize += addedSize
+
+		copy(fileData[d.End+addedSize:], fileData[d.End:])
+		copy(fileData[d.Start:], d.Data)
+
+		if addedSize < 0 {
+			// decrease fileData len
+			fileData = fileData[:len(fileData)+addedSize]
+		}
+	}
+
+	_, err = file.Data().Unmarshal(fileData)
+	return
 }
--- file/uri.go
+++ file/uri.go
@@ -1,12 +1,12 @@
-/* For license and copyright information please see LEGAL file in repository */
+/* For license and copyright information please see the LEGAL file in the code repository */
 
 package file
 
 import (
 	"path/filepath"
 
-	"../convert"
-	"../protocol"
+	"memar/convert"
+	"memar/protocol"
 )
 
 // URI implement protocol.URI and protocol.FileURI interface
@@ -35,16 +35,46 @@ func (uri *URI) Extension() string            { return uri.extension }
 func (uri *URI) IsDirectory() bool            { return IsPathDirectory(uri.path) }
 func (uri *URI) PathParts() []string          { return FilePathParts(uri.path) }
 
-func IsPathDirectory(uriPath string) bool { return uriPath[len(uriPath)-1] == '/' }
-
 // Rename just rename the file name
 func (uri *URI) Rename(newName string) {
 	uri.name = newName
-	for i := len(newName) - 1; i >= 0; i-- {
-		if newName[i] == '.' {
-			uri.extension = newName[i+1:]
+	uri.extension = Extension(newName)
+}
+
+//memar:impl memar/protocol.Codec
+func (uri *URI) MediaType() protocol.MediaType       { return &MediaType }
+func (uri *URI) CompressType() protocol.CompressType { return nil }
+
+// Marshal encodes and return whole file data.
+func (uri *URI) Marshal() (data []byte, err protocol.Error) {
+	var ln = 5
+	data = make([]byte, 0, ln)
+	return
+}
+
+// MarshalTo encodes whole file data to given data and return it with new len.
+func (uri *URI) MarshalTo(data []byte) (added []byte, err protocol.Error) {
+	return data, nil
+}
+
+// Unmarshal save given data to the file. It will overwritten exiting data.
+func (uri *URI) Unmarshal(data []byte) (n int, err protocol.Error) {
+	var dataAsString = convert.UnsafeByteSliceToString(data)
+	var dir, fileName = filepath.Split(dataAsString)
+	uri.path = dir
+	uri.name = fileName
+	// TODO::: decode more data
+	return
+}
+
+func Extension(name string) (ex string) {
+	for i := len(name) - 1; i >= 0; i-- {
+		if name[i] == '.' {
+			ex = name[i+1:]
+			break
 		}
 	}
+	return
 }
 
 func FilePathParts(uriPath string) (parts []string) {
@@ -60,31 +90,4 @@ func FilePathParts(uriPath string) (parts []string) {
 	return
 }
 
-/*
-********** protocol.Codec interface **********
- */
-
-func (uri *URI) MediaType() string    { return "application/uri" }
-func (uri *URI) CompressType() string { return "" }
-
-// Marshal enecodes and return whole file data!
-func (uri *URI) Marshal() (data []byte) {
-	var ln = 5
-	data = make([]byte, 0, ln)
-	return
-}
-
-// MarshalTo enecodes whole file data to given data and return it with new len!
-func (uri *URI) MarshalTo(data []byte) []byte {
-	return data
-}
-
-// Unmarshal save given data to the file. It will overwritten exiting data.
-func (uri *URI) Unmarshal(data []byte) (err protocol.Error) {
-	var dataAsString = convert.UnsafeByteSliceToString(data)
-	var dir, fileName = filepath.Split(dataAsString)
-	uri.path = dir
-	uri.name = fileName
-	// TODO::: decode more data
-	return
-}
+func IsPathDirectory(uriPath string) bool { return uriPath[len(uriPath)-1] == '/' }
