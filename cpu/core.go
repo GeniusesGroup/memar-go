a998ff68d69b4b4e2f1dcc017a542a0af86becf3
Omid Hekayati <omidhekayati@gmail.com>
2023-12-26 15:39:25 +0330

    [cpu] Add more useful functions

--- cpu/core.go
+++ cpu/core.go
@@ -2,5 +2,13 @@
 
 package cpu
 
+import "runtime"
+
 // ActiveCoreID or WhichCoreAmIOn return active core id that thread(goroutine) run on it.
 func ActiveCoreID() uint64
+
+// LogicalCount returns the number of logical CPUs usable by the current process.
+func LogicalCount() uint { return uint(runtime.NumCPU()) }
+
+// PhysicalCount returns the number of physical CPUs usable by the current process.
+func PhysicalCount() uint { return uint(runtime.NumCPU()) }
