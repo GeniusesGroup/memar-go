1746357f0c6cd3b6d768158a21477017cf61403b
Omid Hekayati <omidhekayati@gmail.com>
2024-03-18 19:35:59 +0330

    MOVED some files to `organization` repo

--- achaemenid/manifest_test.go
+++
@@ -1,9 +0,0 @@
-/* For license and copyright information please see the LEGAL file in the code repository */
-
-package achaemenid
-
-import (
-	"memar/protocol"
-)
-
-var _ protocol.ApplicationManifest = &Manifest{}
