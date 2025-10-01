9839f9050eb7b49057ed5d2df8ab20670dd758fe
omidhekayati <omidhekayati@gmail.com>
2025-10-01 19:01:57 +0330

    [process/request] Improved related protocol

--- process/request/protocol/request.go
+++ process/request/protocol/request.go
@@ -1,17 +1,19 @@
 /* For license and copyright information please see the LEGAL file in the code repository */
 
-package operation_p
+package request_p
 
 import (
-	datatype_p "memar/datatype/protocol"
+	codec_p "memar/codec/protocol"
+	datatype_p "memar/computer/datatype/protocol"
 )
 
 // Field_Request ...
 type Field_Request/*[REQ Request]*/ interface {
-	Request() datatype_p.DataType
+	Request() Request
 }
 
 // Request indicate request type that MUST use by any other types.
 type Request interface {
 	datatype_p.DataType
+	codec_p.Codec
 }
