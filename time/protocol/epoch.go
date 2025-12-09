d3c5505fb0e0f650fed91f9177a519a70a3630d0
omidhekayati <omidhekayati@gmail.com>
2025-12-09 15:40:24 +0330

    [time] Add some `Field_` interfaces some minor fixes and improvements

--- time/protocol/epoch.go
+++ time/protocol/epoch.go
@@ -3,14 +3,21 @@
 package time_p
 
 import (
-	datatype_p "memar/datatype/protocol"
-	string_p "memar/string/protocol"
+	datatype_p "memar/computer/datatype/protocol"
+	logic_p "memar/math/logic/protocol"
+	string_p "memar/codec/string/protocol"
 )
+
+type Field_Epoch interface {
+	Epoch() Epoch
+}
 
 // Epoch is the interface that must implement by any time capsule.
 // It is base on Epoch and Second terms to work anywhere (in any planet in the universe).
 // https://en.wikipedia.org/wiki/Epoch
 type Epoch interface {
 	datatype_p.DataType
-	string_p.Stringer_To[string_p.String]
+	string_p.Stringer_To
+
+	logic_p.Equivalence[Epoch]
 }
--- time/protocol/time.go
+++ time/protocol/time.go
@@ -3,19 +3,23 @@
 package time_p
 
 import (
-	string_p "memar/string/protocol"
 	"memar/time/duration"
 )
+
+type Field_Time interface {
+	Time() Time
+}
 
 // Time is the interface that must implement by any time capsule.
 // It is base on Epoch and Second terms to work anywhere (in any planet in the universe).
 type Time interface {
-	Epoch() Epoch
+	Field_Epoch
 	SecondElapsed() duration.Second             // From Epoch
 	NanoInSecondElapsed() duration.NanoInSecond // From second
 }
 
 // Time_Stringer is base on other factor than Time like timezone, ...
 type Time_Stringer[STR string_p.String] interface {
+	// Stringer MUST include factors like timezone, ...
 	string_p.Stringer[STR]
 }
