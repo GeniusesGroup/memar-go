098cfb2446b8abf990918407415a2261ab8102c0
omidhekayati <omidhekayati@gmail.com>
2026-07-02 10:47:13 +0330

    refactor(timer) Moved to `memar-khayyam` due to internal GoLang specs don't let us complete `memar` ideas

--- time/timer/errors/locale.per.go
+++
@@ -1,7 +0,0 @@
-//go:build lang_per
-
-/* For license and copyright information please see the LEGAL file in the code repository */
-
-package errs
-
-const domainPersian = "زمان سنج"
--- time/timer/protocol/timer-status.go
+++
@@ -1,6 +0,0 @@
-/* For license and copyright information please see the LEGAL file in the code repository */
-
-package timer_p
-
-type TimerStatus interface {
-}
